ffc53cc05f5c2fa1c8e5d81cfad107001f74d0e5
amir-shawkat <amirshawkat05@gmail.com>
2025-11-11 01:55:12 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,128 @@
+const addBtn = document.getElementById('add-btn');
+const editBtn = document.getElementById('edit-btn');
+const deleteBtn = document.getElementById('delete-btn');
+
+const addTaskContainer = document.getElementById('add-task-container');
+const taskSubmit = document.getElementById('task-submit');
+const taskTitle = document.getElementById('task-title');
+const taskContainer = document.getElementById('task-container');
+
+const showTask = document.getElementById('show-task')
+
+let checkTask = document.getElementById('check-task');
+
+let taskList = [];
+
+let currentTask = {};
+
+let taskChecked = false;
+
+
+const addOrUpdateTask = () => {
+    if (!taskTitle.value) {
+        alert("Task title cannot be empty."); 
+        return
+    }
+
+
+
+    const taskListIndex = taskList.findIndex((item)=> item.id === currentTask.id);
+
+    const taskObj = {
+        id: `${Date.now()}` ,
+        title: `${taskTitle.value}`,
+        checked: false,
+    }
+
+
+    if(taskListIndex === -1){
+        taskList.unshift(taskObj);
+    } else {
+        taskList[taskListIndex] = taskObj;
+    }
+
+    getTaskList();
+
+    taskTitle.value = "";
+    taskSubmit.innerText = "Submit";
+}
+
+function toggler() {
+    addBtn.classList.toggle('hide');
+    addTaskContainer.classList.toggle('hide');
+}
+
+addBtn.addEventListener("click" , () => {
+    
+    toggler();
+});
+
+taskSubmit.addEventListener("click", ()=>{
+    
+
+    toggler();
+
+    addOrUpdateTask();
+
+
+})
+function getTaskList(){
+
+    taskContainer.innerHTML = '';
+    taskList.forEach(el => {
+        taskContainer.innerHTML += 
+        `
+        <div id="${el.id}">
+            <div class="task-check">
+            <p id="show-task" style="color: ${el.checked ? 'red' : 'inherit'};">${el.title}</p>
+            <input type="checkbox" id="check-task" onchange="checkedTask(${el.id})" ${el.checked ? 'checked' : ''}>
+            </div>
+            <button type="button" id="edit-btn" onclick="editTask(this)">Edit</button>
+            <button type="button" id="delete-btn" onclick="deleteTask(this)">Delete</button>
+        </div>
+        `;
+
+    });
+}
+
+function deleteTask(buttonEl){
+    let isConfirmed = confirm("Are you sure to delete this task?");
+
+    if(isConfirmed){
+        const taskListIndex = taskList.findIndex(item => item.id === buttonEl.parentElement.id);
+   
+        buttonEl.parentElement.remove();
+        taskList.splice(taskListIndex , 1);
+        getTaskList();
+    }
+
+  getTaskList();
+  
+    
+}
+
+function editTask(buttonEl){
+    const taskListIndex = taskList.findIndex(item => item.id === buttonEl.parentElement.id);
+
+    currentTask = taskList[taskListIndex]
+
+    taskTitle.value = currentTask.title;
+
+    toggler();
+
+    taskSubmit.innerText = 'Update Task'
+}
+
+function checkedTask(id) {
+
+    taskList = taskList.map(item => {
+        console.log(item.id == id ? !item.checked : item.checked)
+        return {
+            id: item.id,
+            title: item.title,
+            checked: item.id == id ? !item.checked : item.checked,
+        }
+    })
+    getTaskList();
+}
+
