6c17976800d4306a3533101ce44257b15b0e70a6
amir-shawkat <amirshawkat05@gmail.com>
2025-11-15 23:01:52 +0600

    search functionality added, but some more things to do

--- script.js
+++ script.js
@@ -8,16 +8,31 @@ const cancelSubmit = document.getElementById("cancel-submit");
 
 const clearList = document.getElementById("clear-btn");
 
-// const updateTitle = document.getElementById("update-title");
+const editBtn = document.querySelectorAll(".edit-btn")
+
+const notFound = document.getElementById("not-found-message");
+const searchTask = document.getElementById("search-task");
 
 let taskList = JSON.parse(localStorage.getItem("todoList")) || [];
 
 let currentTask = {};
 
-let taskChecked = false;
-
 getNewTaskList();
 
+searchTask.addEventListener("input" , () => {
+  const searchTerm = searchTask.value.toLowerCase();
+  if (searchTerm) {
+  
+    const filteredTasks = taskList.filter(task => task.title.toLowerCase().includes(searchTerm));
+
+    getSearchTasks(filteredTasks);
+  } else {
+    getNewTaskList();
+  }
+});
+
+
+console.log(taskList)
 function addTask() {
   const taskObj = {
     id: `${Date.now()}`,
@@ -88,6 +103,8 @@ function getNewTaskList() {
 
   taskContainer.replaceChildren();
 
+  searchTask.textContent = ""
+
   taskList.forEach((el) => {
     const listContainer = document.createElement("div");
     listContainer.id = `${el.id}`;
@@ -118,6 +135,7 @@ function getNewTaskList() {
     editButton.classList.add("edit-btn");
     editButton.type = "button";
     editButton.textContent = "Edit";
+    el.checked === true ? editButton.style.display = "none" : editButton.style.display = "block";
 
     const deleteButton = document.createElement("button");
     deleteButton.classList.add("delete-btn");
@@ -197,7 +215,148 @@ function getNewTaskList() {
       moveTask(`${el.id}`, "down");
       getNewTaskList();
     });
+    
+    // if (el.checked) {
+    //   console.log(taskList)
+    //   editBtn.style.display = "none";
+    // } else {
+    //   editBtn.style.display = "block";
+    // }
   });
+}
+
+function getSearchTasks(filter){
+  if(searchTask !== ""){
+    taskContainer.replaceChildren();
+    if(filter.length === 0){
+      notFound.style.display = "block";
+      getNewTaskList();
+    } else {
+      notFound.style.display = "none";
+      // filter.forEach(task => {
+      //   const searchedTaskElement = document.createElement("div");
+      //   searchedTaskElement.classList.add("searched-task-element");
+
+      //   searchedTaskElement.textContent = task.title;
+
+      //   taskContainer.append(searchedTaskElement);
+      // });
+      filter.forEach((el) => {
+      const listContainer = document.createElement("div");
+      listContainer.id = `${el.id}`;
+      listContainer.classList.add("single-task");
+
+      const singleTaskContainer = document.createElement("div");
+      singleTaskContainer.classList.add("task-check");
+
+      const title = document.createElement("p");
+      title.classList.add("show-task");
+      title.textContent = el.title;
+      title.style.color = el.checked ? "#B2B0E899" : "inherit";
+      title.style.textDecoration = el.checked ? "line-through #B2B0E8" : "none";
+
+      const checkboxInput = document.createElement("input");
+      checkboxInput.type = "checkbox";
+      checkboxInput.classList.add("check-task");
+      checkboxInput.checked = el.checked;
+
+      
+
+      singleTaskContainer.append(checkboxInput, title);
+
+      const buttonContainer = document.createElement("div");
+      buttonContainer.classList.add("button-container");
+
+      const editButton = document.createElement("button");
+      editButton.classList.add("edit-btn");
+      editButton.type = "button";
+      editButton.textContent = "Edit";
+      el.checked === true ? editButton.style.display = "none" : editButton.style.display = "block";
+
+      const deleteButton = document.createElement("button");
+      deleteButton.classList.add("delete-btn");
+      deleteButton.type = "button";
+      deleteButton.textContent = "Delete";
+
+      const updateContainer = document.createElement("div");
+      updateContainer.classList.add("update-container");
+      updateContainer.style.display = "none";
+
+      const updateInput = document.createElement("input");
+      updateInput.classList.add("update-title");
+
+      const updateButtonContainer = document.createElement("div");
+
+      const updateButton = document.createElement("button");
+      updateButton.type = "button";
+      updateButton.textContent = "Update";
+
+      const cancelButton = document.createElement("button");
+      cancelButton.type = "button";
+      cancelButton.textContent = "Cancel";
+
+      // const updownContainer = document.createElement("div");
+      // updownContainer.classList.add("updown-container");
+
+      // const upBtn = document.createElement("button");
+      // upBtn.type = "button";
+      // upBtn.textContent = "⇑";
+      // upBtn.classList.add("up-btn");
+
+      // const downBtn = document.createElement("button");
+      // downBtn.type = "button";
+      // downBtn.textContent = "⇓";
+      // downBtn.classList.add("down-btn");
+
+      // updownContainer.append(upBtn, downBtn);
+
+      updateButtonContainer.append(updateButton, cancelButton);
+
+      updateContainer.append(updateInput, updateButtonContainer);
+
+      buttonContainer.append(editButton, deleteButton);
+
+      listContainer.append(singleTaskContainer, buttonContainer);
+
+      taskContainer.append(listContainer, updateContainer);
+
+      checkboxInput.addEventListener("change", () => checkedTask(`${el.id}`));
+
+      editButton.addEventListener("click", () => {
+        updateContainer.style.display = "flex";
+        listContainer.style.display = "none";
+        updateInput.value = `${el.title}`;
+        editTask(`${el.id}`);
+      });
+
+      deleteButton.addEventListener("click", () => deleteTask(`${el.id}`));
+
+      cancelButton.addEventListener("click", () => {
+        updateInput.value = "";
+        currentTask = {};
+        updateContainer.style.display = "none";
+        listContainer.style.display = "flex";
+      });
+
+      updateButton.addEventListener("click", () => {
+        updateTask(updateInput.value);
+        updateContainer.style.display = "none";
+        listContainer.style.display = "flex";
+      });
+
+      // upBtn.addEventListener("click", () => {
+      //   moveTask(`${el.id}`, "up");
+      //   getNewTaskList();
+      // });
+
+      // downBtn.addEventListener("click", () => {
+      //   moveTask(`${el.id}`, "down");
+      //   getNewTaskList();
+      // });
+      
+    });
+    }
+  }
 }
 
 clearList.addEventListener("click", () => {
